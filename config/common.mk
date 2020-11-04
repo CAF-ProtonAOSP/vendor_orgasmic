8c46547e0323e891d459d7109c79ee50ad78a6d2
Danny Lin <danny@kdrag0n.dev>
2020-11-03 22:47:28 -0800

    config: Build simple DeviceConfig provisioning service
    We need a simple service to provision DeviceConfig for setting various
    config defaults, like enabling permission hub, without modifying the
    AOSP source code directly. Build our custom service to do the
    provisioning from resource overlay values instead.
    
    Change-Id: Id2910d799b503c339f56e7f92a5e5f3ba93a5296

--- config/common.mk
+++ config/common.mk
@@ -108,6 +108,10 @@ PRODUCT_PACKAGES += \
     product_charger_res_images
 endif
 
+# Config
+PRODUCT_PACKAGES += \
+    SimpleDeviceConfig
+
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
