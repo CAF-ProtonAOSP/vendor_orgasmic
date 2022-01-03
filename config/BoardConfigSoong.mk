afacf4fade99874119d9a353862ebf68ec4ba397
Travis Holt <travisholt92@gmail.com>
2022-01-03 21:55:07 +0000

    Orgasmic Project:  soong: Dump TARGET_CAMERA_NEEDS_CLIENT_INFO
    https://github.com/AOSP-Krypton/vendor_krypton/commit/616264ff6d430b3632563510e7e43e19a2b2d32c

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -41,6 +41,7 @@ SOONG_CONFIG_orgasmicGlobalVars += \
     target_surfaceflinger_udfps_lib \
     target_uses_prebuilt_dynamic_partitions \
     uses_camera_parameter_lib
+    camera_needs_client_info
 
 SOONG_CONFIG_NAMESPACES += orgasmicNvidiaVars
 SOONG_CONFIG_orgasmicNvidiaVars += \
@@ -70,6 +71,7 @@ endif
 SOONG_CONFIG_orgasmicGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_orgasmicGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 SOONG_CONFIG_orgasmicGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
+SOONG_CONFIG_orgasmicGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_orgasmicGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_orgasmicGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_orgasmicGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
