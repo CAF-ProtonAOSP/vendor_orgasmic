DotOS/android_vendor_dot

vendor: PixelPropsUtils: Add pixel_2016_exclusive.xml
Change-Id: I63e73ee5be5c8d68acea8aa45d7074740a4ec263

config/common.mk
@@ -57,6 +57,10 @@ PRODUCT_COPY_FILES += \
     vendor/dot/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml \
     vendor/dot/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
 
+# PixelPropsUtils
+PRODUCT_COPY_FILES += \
+    vendor/dot/prebuilt/common/etc/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Copy all Dot-specific init rc files
 $(foreach f,$(wildcard vendor/dot/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))

CAF-ProtonAOSP/vendor_orgasmic

Change-Id: Id09a721fbf0cac1cac3248349bb9aa30f0cb8d3c

config/common.mk
@@ -103,6 +103,10 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# PixelPropsUtils
+PRODUCT_COPY_FILES += \
+    vendor/iron/prebuilt/common/etc/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Screen Resolution
 TARGET_SCREEN_WIDTH ?= 1080
 TARGET_SCREEN_HEIGHT ?= 1920